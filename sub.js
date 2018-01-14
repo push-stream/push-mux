9714d8a4547811e83fe06fe35cbbddd9e61b92de
Dominic Tarr <dominic.tarr@gmail.com>
2018-01-14 22:06:13 +1300

    sub should pause if the parent's sink is paused, or there is no sink!

--- sub.js
+++ sub.js
@@ -1,6 +1,10 @@
 var inherits = require('inherits')
 var Stream = require('./stream')
 
+function isError (end) {
+  return end && end !== true
+}
+
 module.exports = Sub
 
 inherits(Sub, Stream)
@@ -9,21 +13,21 @@ function Sub (parent, id) {
   this.parent = parent
   this.id = id
   Stream.call(this)
+  this.paused = false
 }
 
 Sub.prototype.write = function (data) {
   this.parent._write({req: this.id, value: data, stream: true, end: false})
+  this.paused = !this.parent.sink || this.parent.sink.paused
 }
 
 Sub.prototype.end = function (err) {
-  this.parent._write({req: this.id, value: data, stream: true, end: true})
+  this.parent._write({req: this.id, value: err, stream: true, end: true})
   delete this.parent.subs[this.id]
   //if we errored this stream, kill it immediately.
   if(isError(this.ended)) this._end(err)
   //else, if it was and end. wait for the remote to confirm with their error.
 }
-
-
 
 
 
