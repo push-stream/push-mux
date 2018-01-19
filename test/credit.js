1ccdaff1833d9e64aa77ab0be0af8c3e3aca5262
Dominic Tarr <dominic.tarr@gmail.com>
2018-01-19 19:29:38 +1300

    pass credit option (rather than use default) and log stream output

--- test/credit.js
+++ test/credit.js
@@ -8,7 +8,6 @@ var Collect = require('push-stream/collect')
   var array = []
   for(var i = 0; i < 100; i++) array.push(i)
 
-
 function Pauser (N, onEnd) {
   return {
     output: [],
@@ -28,20 +27,44 @@ function Pauser (N, onEnd) {
       this.source.resume()
     }
   }
+}
 
+function Log (name) {
+  return {
+    paused: true,
+    write: function (data) {
+      console.log(name, data)
+      this.sink.write(data)
+      this.paused = data.paused
+    },
+    end: function (err) {
+      this.sink.end(err)
+    },
+    pipe: function (sink) {
+      if(!sink) throw new Error('pipe needs sink')
+      this.sink = sink; sink.source = this
+      this.paused = sink.paused
+      this.resume()
+      return sink
+    },
+    resume: function () {
+      this.source.resume()
+    }
+  }
 }
 
 test('test back pressure on client side', function (t) {
   var _stream
-  var a = new Mux({})
+  var a = new Mux({credit: 10})
   var b = new Mux({
+    credit: 10,
     onStream: function (stream, data) {
       _stream = stream
       stream.name = 'server'
     }
   })
 
-  a.pipe(b).pipe(a)
+  a.pipe(Log('a->b')).pipe(b).pipe(a)
 
   var values = new Values(array.slice())
 
@@ -127,8 +150,6 @@ test('test back pressure through echo server', function (t) {
   pauser.resume()
 })
 
-return
-
 test('test back pressure through echo server', function (t) {
   var _stream
   var a = new Mux({})
@@ -160,4 +181,14 @@ test('test back pressure through echo server', function (t) {
   t.deepEqual(pauser.output.length, 80)
   pauser.resume()
 })
+
+
+
+
+
+
+
+
+
+
 
