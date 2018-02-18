30709982eda179a48863f4c2242be5acb33d9ea4
Dominic Tarr <dominic.tarr@gmail.com>
2018-02-19 00:13:14 +1300

    tests for codec

--- test/codec.js
+++ test/codec.js
@@ -1,6 +1,7 @@
 var Mux = require('../')
 
 var json = {
+  name: 'json',
   encode: function (e) {
     return JSON.stringify(e, null, 2) + '\n\n'
   },
@@ -18,59 +19,36 @@ function Null () {
 
 var test = require('tape')
 
-test('write encoded data to a stream and check that the output length matches the input', function (t) {
+module.exports = function (codec) {
 
-  var CREDIT = 5*1024
-  var b = Mux({codec: json, credit: CREDIT, onStream: function (_stream) { stream = _stream }})
-  var a = Mux({codec: json, credit: CREDIT})
+  test(codec.name +': write encoded data to a stream and check that the output length matches the input', function (t) {
 
-  a.pipe(b).pipe(a)
-  var as = a.stream({})
-  var bs = stream
+    var CREDIT = 5*1024
+    var b = Mux({codec: codec, credit: CREDIT, onStream: function (_stream) { stream = _stream }})
+    var a = Mux({codec: codec, credit: CREDIT})
 
-  while(!as.paused)
-    as.write({random: Math.random(), time: Date.now() })
-  t.ok(as.writes > CREDIT/2)
-  t.ok(as.writes < CREDIT)
-  console.log(bs.buffer)
-  t.equal(bs.reads, 0)
-  bs.pipe(Null())
-  t.notEqual(as.writes, 0)
-  t.equal(bs.reads, as.writes)
+    a.pipe(b).pipe(a)
+    var as = a.stream({})
+    var bs = stream
 
-  while(as.writes < CREDIT*2)
-    as.write({random: Math.random(), time: Date.now() })
-  t.equal(bs.reads, as.writes)
+    while(!as.paused)
+      as.write({random: Math.random(), time: Date.now() })
+    t.ok(as.writes > CREDIT/2)
+    t.ok(as.writes < CREDIT)
+    t.equal(bs.reads, 0)
+    bs.pipe(Null())
+    t.notEqual(as.writes, 0)
+    t.equal(bs.reads, as.writes)
 
-  t.end()
-})
+    while(as.writes < CREDIT*2)
+      as.write({random: Math.random(), time: Date.now() })
+    t.equal(bs.reads, as.writes)
 
-test('write encoded data to a stream and check that the output length matches the input', function (t) {
+    t.end()
+  })
+}
 
-  var CREDIT = 5*1024
-  var b = Mux({codec: json, credit: CREDIT, onStream: function (_stream) { stream = _stream }})
-  var a = Mux({codec: json, credit: CREDIT})
-
-  a.pipe(b).pipe(a)
-  var bs = a.stream({})
-  var as = stream
-
-  while(!as.paused)
-    as.write({random: Math.random(), time: Date.now() })
-
-  t.ok(as.writes > CREDIT/2)
-  t.ok(as.writes < CREDIT)
-  console.log(bs.buffer)
-  t.equal(bs.reads, 0)
-  bs.pipe(Null())
-  t.notEqual(as.writes, 0)
-  t.equal(bs.reads, as.writes)
-
-  while(as.writes < CREDIT*2)
-    as.write({random: Math.random(), time: Date.now() })
-  t.equal(bs.reads, as.writes)
-
-  t.end()
-})
+module.exports(json)
+module.exports(require('../../packet-stream-codec/codec'))
 
 
